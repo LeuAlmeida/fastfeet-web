e1630101078d95527a394da17f88c8a767664706
LeuAlmeida <leo@webid.net.br>
2020-02-19 17:34:15 -0300

    refactor(pages): update and refactor pages

--- src/pages/Profile/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-function Profile() {
-  return <div />;
-}
-
-export default Profile;
--- src/pages/SignUp/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-function SignUp() {
-  return <div />;
-}
-
-export default SignUp;
