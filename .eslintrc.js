5eaafc213a6c1c645c97cafcfc15e73185f67b8f
LeuAlmeida <leo@webid.net.br>
2020-02-19 13:36:39 -0300

    chore(eslint): remove settings to now

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,36 @@
 module.exports = {
   env: {
+    browser: true,
     es6: true,
-    jest: true,
-    browser: true
   },
-  extends: ["airbnb", "prettier", "prettier/react"],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-    __DEV__: true
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
-  plugins: ["react", "jsx-a11y", "import", "react-hooks", "prettier"],
+  plugins: [
+    'react',
+    'prettier',
+  ],
   rules: {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js'] }
+    ],
     "import/prefer-default-export": "off",
     "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     "react/jsx-one-expression-per-line": "off",
@@ -33,11 +43,11 @@ module.exports = {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn"
   },
-  settings: {
-    "import/resolver": {
-      "babel-plugin-root-import": {
-        rootPathSuffix: "src"
-      },
-    },
-  },
+  // settings: {
+  //   "import/resolver": {
+  //     "babel-plugin-root-import": {
+  //       rootPathSuffix: "src"
+  //     },
+  //   },
+  // },
 };
