c3b485a26c746a18c292a0573c8387cebbd6283a
LeuAlmeida <leo@webid.net.br>
2020-02-20 19:30:16 -0300

    style(auth): create login template screen

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -1,6 +1,84 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: linear-gradient(-90deg, #7159c1, #ab59c1);
+  background: #816fe7;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding: 30px 25px;
+  text-align: center;
+  background: #fff;
+  border-radius: 4px;
+
+  img {
+    width: 100%;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    label {
+      color: #333;
+      align-self: flex-start;
+      margin: 10px 0;
+      text-transform: uppercase;
+      font-weight: bold;
+    }
+
+    input {
+      border: 1px solid rgba(0, 0, 0, 0.1);
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      color: #fff;
+      margin: 0 0 10px;
+
+      &::placeholder {
+        color: rgba(0, 0, 0, 0.4);
+      }
+    }
+
+    span {
+      color: #fb6f91;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
+    }
+
+    button {
+      margin: 5px 0 0;
+      height: 44px;
+      background: #816fe7;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.1, '#816fe7')};
+      }
+    }
+
+    a {
+      color: #fff;
+      margin-top: 15px;
+      font-size: 16px;
+      opacity: 0.8;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
 `;
