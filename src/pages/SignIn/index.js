7244e7a74ddfd2301c6b86436c6bf83a9ea0465f
LeuAlmeida <leo@webid.net.br>
2020-02-20 19:29:56 -0300

    feat(signin): provide Login page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,34 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 function SignIn() {
-  return <h1>Login</h1>;
+  function handleSubmit(data) {}
+
+  return (
+    <>
+      <img src={logo} alt="Gobarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <label>Seu e-mail</label>
+        <Input name="email" type="email" placeholder="exemplo@email.com" />
+        <label>Sua senha</label>
+        <Input name="password" type="password" placeholder="********" />
+
+        <button type="submit">Entrar no sistema</button>
+      </Form>
+    </>
+  );
 }
 
 export default SignIn;
