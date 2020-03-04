ccf52eceb4427b213a2b5c7d2159630cb6fba47d
LeuAlmeida <leo@webid.net.br>
2020-03-04 13:12:05 -0300

    chore(Delivery): implementing Delivery component

--- src/pages/Delivery/index.js
+++ src/pages/Delivery/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { MdAdd } from 'react-icons/md';
 
-// import { Container } from './styles';
+import IconButton from '~/components/utils/Button/IconButton';
+import HeaderList from '~/components/HeaderList';
+
+import history from '~/services/history';
+
+import { Container, Content, Grid } from './styles';
 
 export default function Delivery() {
-  return <h1>Delivery</h1>;
+  return (
+    <Container>
+      <Content>
+        <HeaderList title="Gerenciando encomendas">
+          {/* <SearchInput
+            onChange={handleSearchDelivery}
+            type="text"
+            placeholder="Buscar por encomendas"
+          /> */}
+          <IconButton
+            Icon={MdAdd}
+            title="CADASTRAR"
+            action={() => history.push('/deliveries/form')}
+            type="button"
+          />
+        </HeaderList>
+
+        <Grid>
+          <section>
+            <strong>ID</strong>
+            <strong>Destinatário</strong>
+            <strong>Produto</strong>
+            <strong>Cidade</strong>
+            <strong>Estado</strong>
+            <strong>Status</strong>
+            <strong>Ações</strong>
+          </section>
+        </Grid>
+      </Content>
+    </Container>
+  );
 }
