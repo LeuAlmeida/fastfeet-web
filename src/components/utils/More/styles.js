2881cc378a3fb2d89076a4cb23874ff7f2ef36cc
LeuAlmeida <leo@webid.net.br>
2020-03-04 14:53:59 -0300

    style(More): create Button styles

--- src/components/utils/More/styles.js
+++ src/components/utils/More/styles.js
@@ -1,3 +1,10 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Button = styled.button`
+  background: none;
+  border: none;
+  height: 25px;
+  width: 20px;
+  align-self: center;
+  margin-left: 75%;
+`;
