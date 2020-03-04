d0b828e1d07da636d6432505fac7775a8b85e97a
LeuAlmeida <leo@webid.net.br>
2020-03-04 13:06:37 -0300

    chore(Delivery): remove unusuless pages

--- src/pages/Delivery/Create.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-export default function CreateDelivery() {
-  return <h1>Create Delivery</h1>;
-}
--- src/pages/Delivery/Update.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-export default function UpdateDelivery() {
-  return <h1>Update Delivery</h1>;
-}
