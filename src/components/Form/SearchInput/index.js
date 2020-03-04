4a13282fbe0c79053e9127e0d93007bf4a538368
LeuAlmeida <leo@webid.net.br>
2020-03-04 13:20:27 -0300

    feat(SearchInput): improving component

--- src/components/Form/SearchInput/index.js
+++ src/components/Form/SearchInput/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { MdSearch } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
-export default function SearchInput() {
-  return <div />;
+export default function SearchInput({ ...rest }) {
+  return (
+    <Container>
+      <MdSearch color="#999" size={16} />
+      <input type="text" {...rest} />
+    </Container>
+  );
 }
