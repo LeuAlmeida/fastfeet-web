055015309758d6946ef4204dede676b98d3b15de
LeuAlmeida <leo@webid.net.br>
2020-03-04 12:47:07 -0300

    chore(sagas): remove user saga

--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -1,7 +1,6 @@
 import { all } from 'redux-saga/effects';
 import auth from './auth/sagas';
-import user from './user/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth, user]);
+  return yield all([auth]);
 }
