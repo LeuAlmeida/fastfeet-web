35e7277d6ca361469842f84aba7aaab11da59804
LeuAlmeida <leo@webid.net.br>
2020-03-04 13:19:18 -0300

    feat(Delivery): create Delivery header component

--- src/pages/Delivery/index.js
+++ src/pages/Delivery/index.js
@@ -3,21 +3,24 @@ import { MdAdd } from 'react-icons/md';
 
 import IconButton from '~/components/utils/Button/IconButton';
 import HeaderList from '~/components/HeaderList';
+import SearchInput from '~/components/Form/SearchInput';
 
 import history from '~/services/history';
 
 import { Container, Content, Grid } from './styles';
 
 export default function Delivery() {
+  function handleSearchDelivery(e) {}
+
   return (
     <Container>
       <Content>
         <HeaderList title="Gerenciando encomendas">
-          {/* <SearchInput
+          <SearchInput
             onChange={handleSearchDelivery}
             type="text"
             placeholder="Buscar por encomendas"
-          /> */}
+          />
           <IconButton
             Icon={MdAdd}
             title="CADASTRAR"
