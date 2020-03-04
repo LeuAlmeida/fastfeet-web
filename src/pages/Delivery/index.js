20cf83ff7092e24654967707a405e5a221706d9c
LeuAlmeida <leo@webid.net.br>
2020-03-04 18:48:40 -0300

    feat(Delivery): create Delivery list

--- src/pages/Delivery/index.js
+++ src/pages/Delivery/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { MdAdd } from 'react-icons/md';
+import { MdAdd, MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { parseISO, format } from 'date-fns';
 
 import IconButton from '~/components/utils/Button/IconButton';
 import HeaderList from '~/components/HeaderList';
@@ -15,7 +16,47 @@ export default function Delivery() {
   const [deliveries, setDeliveries] = useState([]);
   const [page, setPage] = useState(1);
 
-  function handleSearchDelivery(e) {}
+  function formatDate(data) {
+    return data.map(delivery => ({
+      ...delivery,
+      start_dateFormated: delivery.start_date
+        ? format(parseISO(delivery.start_date), 'dd/MM/yyyy')
+        : null,
+      end_dateFormated: delivery.end_date
+        ? format(parseISO(delivery.end_date), 'dd/MM/yyyy')
+        : null,
+    }));
+  }
+
+  async function handleSearchDelivery(e) {
+    setPage(1);
+    const response = await api.get('/delivery', {
+      params: {
+        q: e.target.value,
+        page,
+      },
+    });
+
+    const data = formatDate(response.data);
+
+    setDeliveries(data);
+  }
+
+  async function loadDeliveries() {
+    const response = await api.get('/delivery', {
+      params: {
+        page,
+      },
+    });
+
+    const data = formatDate(response.data);
+
+    setDeliveries(data);
+  }
+
+  useEffect(() => {
+    loadDeliveries();
+  }, [page]);
 
   return (
     <Container>
@@ -44,7 +85,30 @@ export default function Delivery() {
             <strong>Status</strong>
             <strong>Ações</strong>
           </section>
+          {deliveries.map(delivery => (
+            <DeliveryItem
+              updateDeliveries={loadDeliveries}
+              key={delivery.id}
+              data={delivery}
+            />
+          ))}
         </Grid>
+        <section>
+          <Button
+            disabled={page === 1}
+            onClick={() => setPage(page - 1)}
+            type="button"
+          >
+            <MdChevronLeft size={26} />
+          </Button>
+          <Button
+            disabled={deliveries.length < 5}
+            type="button"
+            onClick={() => setPage(page + 1)}
+          >
+            <MdChevronRight size={26} />
+          </Button>
+        </section>
       </Content>
     </Container>
   );
