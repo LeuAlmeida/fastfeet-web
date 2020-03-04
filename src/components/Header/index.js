30ca4f298bcaec4cf85c7aab2d11dcf8a50807e9
LeuAlmeida <leo@webid.net.br>
2020-03-04 12:44:22 -0300

    feat(Header): create component's initial structure

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { Container, Content, Navigation, Profile } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/logo.png';
 
 export default function Header() {
-  return <div />;
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="FastFeet" />
+          <Navigation>
+            <NavLink to="/delivery">ENCOMENDAS</NavLink>
+            <NavLink to="/deliveryman">ENTREGADORES</NavLink>
+            <NavLink to="/recipients">DESTINATÁRIOS</NavLink>
+            <NavLink to="/problems">PROBLEMAS</NavLink>
+          </Navigation>
+        </nav>
+
+        <aside>
+          <Profile>
+            <strong>Léu Almeida</strong>
+
+            <button type="button">sair do sistema</button>
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
 }
