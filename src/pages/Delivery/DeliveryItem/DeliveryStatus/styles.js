0de29f356fd2d1b370618e6622d44f4320903da1
LeuAlmeida <leo@webid.net.br>
2020-03-05 12:24:39 -0300

    style(DeliveryStatus): limit container width

--- src/pages/Delivery/DeliveryItem/DeliveryStatus/styles.js
+++ src/pages/Delivery/DeliveryItem/DeliveryStatus/styles.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
 
-  justify-content: flex-start;
+  width: 80px;
+  justify-content: flex-end;
   align-items: center;
 `;
 
