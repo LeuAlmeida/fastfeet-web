65ce353697e4ff07147289f4389c392c993a89d1
LeuAlmeida <leo@webid.net.br>
2020-03-06 11:06:31 -0300

    feat(Delivery): create loading statement

--- src/pages/Delivery/index.js
+++ src/pages/Delivery/index.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { MdAdd, MdChevronLeft, MdChevronRight, MdBlock } from 'react-icons/md';
+import {
+  MdAdd,
+  MdChevronLeft,
+  MdChevronRight,
+  MdBlock,
+  MdCached,
+} from 'react-icons/md';
 import { parseISO, format } from 'date-fns';
 
 import IconButton from '~/components/utils/Button/IconButton';
@@ -8,6 +14,7 @@ import SearchInput from '~/components/Form/SearchInput';
 
 import DeliveryItem from './DeliveryItem';
 import {
+  LoadingField,
   Container,
   Content,
   Grid,
@@ -24,6 +31,7 @@ import history from '~/services/history';
 export default function Delivery() {
   const [deliveries, setDeliveries] = useState([]);
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   function formatDate(data) {
     return data.map(delivery => ({
@@ -38,6 +46,7 @@ export default function Delivery() {
   }
 
   async function handleSearchDelivery(e) {
+    setLoading(true);
     setPage(1);
     const response = await api.get('/delivery', {
       params: {
@@ -49,9 +58,11 @@ export default function Delivery() {
     const data = formatDate(response.data);
 
     setDeliveries(data);
+    setLoading(false);
   }
 
   async function loadDeliveries() {
+    setLoading(true);
     const response = await api.get('/delivery', {
       params: {
         page,
@@ -61,11 +72,68 @@ export default function Delivery() {
     const data = formatDate(response.data);
 
     setDeliveries(data);
+    setLoading(false);
   }
 
   useEffect(() => {
     loadDeliveries();
   }, [page]);
+
+  if (loading) {
+    return (
+      <Container>
+        <Content>
+          <HeaderList title="Gerenciando encomendas">
+            <SearchInput
+              onChange={handleSearchDelivery}
+              type="text"
+              placeholder="Buscar por encomendas"
+            />
+            <IconButton
+              Icon={MdAdd}
+              title="CADASTRAR"
+              action={() => history.push('/delivery/create')}
+              type="button"
+            />
+          </HeaderList>
+
+          <Grid null={!deliveries.length > 0}>
+            <section>
+              <strong>ID</strong>
+              <strong>Destinatário</strong>
+              <strong>Entregador</strong>
+              <strong>Cidade</strong>
+              <strong>Estado</strong>
+              <strong>Status</strong>
+              <strong>Ações</strong>
+            </section>
+            <LoadingField>
+              <MdCached size={86} color={colors.primary} />
+              <span>Carregando...</span>
+            </LoadingField>
+          </Grid>
+          <ButtonSection>
+            <Button
+              disabled={page === 1}
+              onClick={() => setPage(page - 1)}
+              type="button"
+            >
+              <MdChevronLeft size={26} />{' '}
+              <span style={{ marginRight: 5 }}>Voltar</span>
+            </Button>
+            <Button
+              disabled={deliveries.length < 5}
+              type="button"
+              onClick={() => setPage(page + 1)}
+            >
+              <span style={{ marginLeft: 5 }}>Próximo</span>
+              <MdChevronRight size={26} />
+            </Button>
+          </ButtonSection>
+        </Content>
+      </Container>
+    );
+  }
 
   return (
     <Container>
