e73b4e4b32a6db4e84bb37ec79ac1151e2409e75
LeuAlmeida <leo@webid.net.br>
2020-03-06 11:07:14 -0300

    chore(Deliveryman): delete unusuless pages

--- src/pages/Deliveryman/Create.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-export default function CreateDeliveryman() {
-  return <h1>Create Deliveryman</h1>;
-}
--- src/pages/Deliveryman/Update.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-export default function UpdateDeliveryman() {
-  return <h1>Update Deliveryman</h1>;
-}
