7a5a437eb8af6ff8940fb284f2baf6a8750544be
LeuAlmeida <leo@webid.net.br>
2020-03-09 18:27:09 -0300

    feat(Deliveryman): create loading effect

--- src/pages/Deliveryman/index.js
+++ src/pages/Deliveryman/index.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { MdAdd } from 'react-icons/md';
+import {
+  MdAdd,
+  MdChevronLeft,
+  MdChevronRight,
+  MdBlock,
+  MdCached,
+  MdError,
+} from 'react-icons/md';
 
 import IconButton from '~/components/utils/Button/IconButton';
 import SearchInput from '~/components/Form/SearchInput';
@@ -7,21 +14,39 @@ import HeaderList from '~/components/HeaderList';
 import api from '~/services/api';
 import history from '~/services/history';
 
+import { colors } from '~/styles/colors';
+
 import DeliverymanItem from './DeliverymanItem';
-import { Container, Content, Grid, Button } from './styles';
+import {
+  Container,
+  Content,
+  Grid,
+  ButtonSection,
+  Button,
+  LoadingField,
+} from './styles';
 
 export default function Deliverymen() {
   const [deliverymen, setDeliverymen] = useState([]);
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   async function loadDeliverymen() {
-    const response = await api.get('/deliveryman', {
-      params: {
-        page,
-      },
-    });
+    setLoading(true);
+    try {
+      const response = await api.get('/deliverymaan', {
+        params: {
+          page,
+        },
+      });
 
-    setDeliverymen(response.data);
+      setDeliverymen(response.data);
+      setLoading(false);
+    } catch (err) {
+      setError(err);
+      setLoading(false);
+    }
   }
 
   useEffect(() => {
@@ -30,15 +55,76 @@ export default function Deliverymen() {
 
   async function handleSearchDeliveryman(e) {
     setPage(1);
+    setLoading(true);
 
-    const response = await api.get('/deliveryman', {
-      params: {
-        q: e.target.value,
-        page,
-      },
-    });
+    try {
+      const response = await api.get('/deliveryman', {
+        params: {
+          q: e.target.value,
+          page,
+        },
+      });
 
-    setDeliverymen(response.data);
+      setDeliverymen(response.data);
+      setLoading(false);
+    } catch (err) {
+      setError(err);
+      setLoading(false);
+    }
+  }
+
+  if (loading) {
+    return (
+      <Container>
+        <Content>
+          <HeaderList title="Gerenciando entregadores">
+            <SearchInput
+              onChange={handleSearchDeliveryman}
+              type="text"
+              placeholder="Buscar por entregadores"
+            />
+            <IconButton
+              Icon={MdAdd}
+              title="CADASTRAR"
+              action={() => history.push('/deliveryman/create')}
+              type="button"
+            />
+          </HeaderList>
+
+          <Grid null={!deliverymen.length > 0}>
+            <section>
+              <strong>ID</strong>
+              <strong>Foto</strong>
+              <strong>Nome</strong>
+              <strong>Email</strong>
+              <strong>Ações</strong>
+            </section>
+            <LoadingField>
+              <MdCached size={86} color={colors.primary} />
+              <span>Carregando...</span>
+            </LoadingField>
+          </Grid>
+          <ButtonSection>
+            <Button
+              disabled={page === 1}
+              onClick={() => setPage(page - 1)}
+              type="button"
+            >
+              <MdChevronLeft size={26} />{' '}
+              <span style={{ marginRight: 5 }}>Voltar</span>
+            </Button>
+            <Button
+              disabled={deliverymen.length < 5}
+              type="button"
+              onClick={() => setPage(page + 1)}
+            >
+              <span style={{ marginLeft: 5 }}>Próximo</span>
+              <MdChevronRight size={26} />
+            </Button>
+          </ButtonSection>
+        </Content>
+      </Container>
+    );
   }
 
   return (
@@ -74,22 +160,24 @@ export default function Deliverymen() {
             />
           ))}
         </Grid>
-        <section>
+        <ButtonSection>
           <Button
             disabled={page === 1}
             onClick={() => setPage(page - 1)}
             type="button"
           >
-            voltar
+            <MdChevronLeft size={26} />{' '}
+            <span style={{ marginRight: 5 }}>Voltar</span>
           </Button>
           <Button
             disabled={deliverymen.length < 5}
             type="button"
             onClick={() => setPage(page + 1)}
           >
-            proximo
+            <span style={{ marginLeft: 5 }}>Próximo</span>
+            <MdChevronRight size={26} />
           </Button>
-        </section>
+        </ButtonSection>
       </Content>
     </Container>
   );
