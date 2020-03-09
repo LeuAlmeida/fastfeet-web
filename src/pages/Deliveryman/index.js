6daae67fcb6bf520d41341583ce57bc4a931d831
LeuAlmeida <leo@webid.net.br>
2020-03-09 11:58:44 -0300

    feat(Deliveryman): create initial component structure

--- src/pages/Deliveryman/index.js
+++ src/pages/Deliveryman/index.js
@@ -1,7 +1,89 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { MdAdd } from 'react-icons/md';
 
-// import { Container } from './styles';
+import IconButton from '~/components/utils/Button/IconButton';
+import SearchInput from '~/components/Form/SearchInput';
+import HeaderList from '~/components/HeaderList';
+import api from '~/services/api';
+import history from '~/services/history';
 
-export default function Deliveryman() {
-  return <h1>Deliveryman</h1>;
+// import DeliverymanItem from './DeliverymanItem';
+import { Container, Content, Grid, Button } from './styles';
+
+export default function Deliverymen() {
+  const [deliverymans, setDeliverymen] = useState([]);
+  const [page, setPage] = useState(1);
+
+  async function loadDeliverymen() {
+    const response = await api.get('/deliveryman', {
+      params: {
+        page,
+      },
+    });
+
+    setDeliverymen(response.data);
+  }
+
+  useEffect(() => {
+    loadDeliverymen();
+  }, [page]);
+
+  async function handleSearchDeliveryman(e) {
+    setPage(1);
+
+    const response = await api.get('/deliveryman', {
+      params: {
+        q: e.target.value,
+        page,
+      },
+    });
+
+    setDeliverymen(response.data);
+  }
+
+  return (
+    <Container>
+      <Content>
+        <HeaderList title="Gerenciando entregadores">
+          <SearchInput
+            onChange={handleSearchDeliveryman}
+            type="text"
+            placeholder="Buscar por entregadores"
+          />
+          <IconButton
+            Icon={MdAdd}
+            title="CADASTRAR"
+            action={() => history.push('/deliveryman/create')}
+            type="button"
+          />
+        </HeaderList>
+
+        <Grid>
+          <section>
+            <strong>ID</strong>
+            <strong>Foto</strong>
+            <strong>Nome</strong>
+            <strong>Email</strong>
+            <strong>Ações</strong>
+          </section>
+        </Grid>
+        <section>
+          <Button
+            disabled={page === 1}
+            onClick={() => setPage(page - 1)}
+            type="button"
+          >
+            voltar
+          </Button>
+          <Button
+            disabled={deliverymans.length < 5}
+            type="button"
+            onClick={() => setPage(page + 1)}
+          >
+            proximo
+          </Button>
+        </section>
+      </Content>
+    </Container>
+  );
 }
