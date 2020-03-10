c6806b7af54a27251992e856befe35da03282685
LeuAlmeida <leo@webid.net.br>
2020-03-10 11:15:11 -0300

    feat(PhotoInput): improving component's structure

--- src/components/Form/PhotoInput/index.js
+++ src/components/Form/PhotoInput/index.js
@@ -1,22 +1,65 @@
-import React from 'react';
+import React, { useRef, useEffect, useCallback, useState } from 'react';
+import { MdInsertPhoto } from 'react-icons/md';
 
+import { useField } from '@unform/core';
 import PropTypes from 'prop-types';
 
-import { Container } from './styles';
+import { Container, Content } from './styles';
 
-export default function NamePhoto({ name }) {
-  const nameSplit = name.split(' ');
+export default function PhotoInput({ name, ...rest }) {
+  const inputRef = useRef(null);
+  const { fieldName, registerField, defaultValue } = useField(name);
+  const [preview, setPreview] = useState(defaultValue);
+
+  const handlePreview = useCallback(e => {
+    const file = e.target.files?.[0];
+    if (!file) {
+      setPreview(null);
+      return;
+    }
+
+    const previewURL = URL.createObjectURL(file);
+    setPreview(previewURL);
+  }, []);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'files[0]',
+      clearValue(ref) {
+        ref.value = '';
+        setPreview(null);
+      },
+      setValue(_, value) {
+        setPreview(value);
+      },
+    });
+  }, [fieldName, registerField]);
 
   return (
-    <Container number={Math.floor(Math.random() * (5 + 1))}>
-      <span>
-        {nameSplit?.[0]?.[0]}
-        {nameSplit?.[1]?.[0]}
-      </span>
+    <Container>
+      <Content htmlFor="avatar">
+        {preview ? (
+          <img src={preview} alt="Preview" width="150" />
+        ) : (
+          <>
+            <MdInsertPhoto size={40} color="#ddd" />
+            <strong>Adicionar foto</strong>
+          </>
+        )}
+        <input
+          id="avatar"
+          type="file"
+          ref={inputRef}
+          onChange={handlePreview}
+          {...rest}
+        />
+      </Content>
     </Container>
   );
 }
 
-NamePhoto.propTypes = {
+PhotoInput.propTypes = {
   name: PropTypes.string.isRequired,
 };
