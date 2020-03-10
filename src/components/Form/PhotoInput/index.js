0006bed71dbc7225d64841c65bcbacf8167976b7
LeuAlmeida <leo@webid.net.br>
2020-03-10 10:47:22 -0300

    feat(PhotoInput): improving structure

--- src/components/Form/PhotoInput/index.js
+++ src/components/Form/PhotoInput/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import PropTypes from 'prop-types';
 
-export default function PhotoInput() {
-  return <div />;
+import { Container } from './styles';
+
+export default function NamePhoto({ name }) {
+  const nameSplit = name.split(' ');
+
+  return (
+    <Container number={Math.floor(Math.random() * (5 + 1))}>
+      <span>
+        {nameSplit?.[0]?.[0]}
+        {nameSplit?.[1]?.[0]}
+      </span>
+    </Container>
+  );
 }
+
+NamePhoto.propTypes = {
+  name: PropTypes.string.isRequired,
+};
