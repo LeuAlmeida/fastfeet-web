da02cf64fc2f532923153de6990f37001b97126d
LeuAlmeida <leo@webid.net.br>
2020-03-11 12:11:19 -0300

    feat(Recipients): create initial structure

--- src/pages/Recipients/index.js
+++ src/pages/Recipients/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import HeaderList from '~/components/HeaderList';
 
-// import { Container } from './styles';
+import { Container, Content, Grid, Button } from './styles';
 
 export default function Recipients() {
-  return <h1>Recipients</h1>;
+  return (
+    <Container>
+      <Content>
+        <HeaderList title="Gerenciando destinatários" />
+        <Grid>
+          <section>
+            <strong>ID</strong>
+            <strong>Nome</strong>
+            <strong>Endereço</strong>
+            <strong>Ações</strong>
+          </section>
+        </Grid>
+        {/* <section>
+          <Button
+            disabled={page === 1}
+            onClick={() => setPage(page - 1)}
+            type="button"
+          >
+            voltar
+          </Button>
+          <Button
+            disabled={recipients.length < 5}
+            type="button"
+            onClick={() => setPage(page + 1)}
+          >
+            proximo
+          </Button>
+        </section> */}
+      </Content>
+    </Container>
+  );
 }
