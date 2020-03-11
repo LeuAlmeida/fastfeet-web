1a19a98c67c40854670794539c937b0a69d20909
LeuAlmeida <leo@webid.net.br>
2020-03-11 12:16:04 -0300

    feat(Recipients): improving structure

--- src/pages/Recipients/index.js
+++ src/pages/Recipients/index.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  MdAdd,
+  MdChevronLeft,
+  MdChevronRight,
+  MdBlock,
+  MdCached,
+  MdError,
+} from 'react-icons/md';
+
+import api from '~/services/api';
+
 import HeaderList from '~/components/HeaderList';
 
-import { Container, Content, Grid, Button } from './styles';
+import { Container, Content, Grid, ButtonSection, Button } from './styles';
 
 export default function Recipients() {
+  const [page, setPage] = useState(1);
+  const [recipients, setRecipients] = useState([]);
+
+  async function loadRecipients() {
+    const response = await api.get('/recipients', {
+      params: {
+        page,
+      },
+    });
+
+    setRecipients(response.data);
+  }
+
+  useEffect(() => {
+    loadRecipients();
+	}, [page]); // eslint-disable-line
+
+  async function handleSearchRecipient(e) {
+    setPage(1);
+
+    const response = await api.get('/recipients', {
+      params: {
+        q: e.target.value,
+        page,
+      },
+    });
+
+    setRecipients(response.data);
+  }
+
   return (
     <Container>
       <Content>
@@ -16,22 +57,24 @@ export default function Recipients() {
             <strong>Ações</strong>
           </section>
         </Grid>
-        {/* <section>
+        <ButtonSection>
           <Button
             disabled={page === 1}
             onClick={() => setPage(page - 1)}
             type="button"
           >
-            voltar
+            <MdChevronLeft size={26} />{' '}
+            <span style={{ marginRight: 5 }}>Voltar</span>
           </Button>
           <Button
             disabled={recipients.length < 5}
             type="button"
             onClick={() => setPage(page + 1)}
           >
-            proximo
+            <span style={{ marginLeft: 5 }}>Próximo</span>
+            <MdChevronRight size={26} />
           </Button>
-        </section> */}
+        </ButtonSection>
       </Content>
     </Container>
   );
