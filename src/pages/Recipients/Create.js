db5cdba00edb9405c2660f2e298a3d0eb3984dda
LeuAlmeida <leo@webid.net.br>
2020-03-11 12:04:49 -0300

    chore(Recipients): remove unusuless components

--- src/pages/Recipients/Create.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-export default function CreateRecipients() {
-  return <h1>Create Recipients</h1>;
-}
--- src/pages/Recipients/Update.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-export default function UpdateRecipients() {
-  return <h1>Update Recipients</h1>;
-}
