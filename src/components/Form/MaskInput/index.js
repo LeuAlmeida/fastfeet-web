c2c386d082fec43089e3a5cb64ed5b7a65019f3e
LeuAlmeida <leo@webid.net.br>
2020-03-12 12:39:29 -0300

    feat(MaskInput): implementing functional component

--- src/components/Form/MaskInput/index.js
+++ src/components/Form/MaskInput/index.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
-// import { Container } from './styles';
+import { useField } from '@unform/core';
 
-export default function MaskInput() {
-  return <div />;
+import PropTypes from 'prop-types';
+
+import { InputMask, Label, Error } from './styles';
+
+export default function MaskInput({ name, label, ...rest }) {
+  const inputRef = useRef(null);
+  const { fieldName, registerField, defaultValue, error } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+      setValue(ref, value) {
+        ref.value = value;
+      },
+      clearValue(ref) {
+        ref.value = '';
+      },
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Label htmlFor={fieldName}>
+      <strong>{label}</strong>
+      <InputMask ref={inputRef} defaultValue={defaultValue} {...rest} />
+      {error && <Error>{error}</Error>}
+    </Label>
+  );
 }
+
+MaskInput.propTypes = {
+  name: PropTypes.string,
+  label: PropTypes.string,
+};
+
+MaskInput.defaultProps = {
+  name: '',
+  label: '',
+};
