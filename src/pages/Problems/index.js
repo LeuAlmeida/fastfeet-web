a8bd029dfb968780ab4921fa58443111d3fdc6da
LeuAlmeida <leo@webid.net.br>
2020-03-13 12:58:01 -0300

    feat(Problems): create component

--- src/pages/Problems/index.js
+++ src/pages/Problems/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// import { Container } from './styles';
+import HeaderList from '~/components/HeaderList';
+
+import { Container, Content, Grid, Button } from './styles';
 
 export default function Problems() {
-  return <div />;
+  const [page, setPage] = useState(1);
+
+  return (
+    <Container>
+      <Content>
+        <HeaderList title="Problemas na entrega" />
+        <Grid>
+          <section>
+            <strong>Encomenda</strong>
+            <strong>Problema</strong>
+            <strong>Ações</strong>
+          </section>
+        </Grid>
+        <section>
+          <Button
+            disabled={page === 1}
+            onClick={() => setPage(page - 1)}
+            type="button"
+          >
+            voltar
+          </Button>
+          {/* <Button
+						disabled={problems.length < 5}
+						type="button"
+						onClick={() => setPage(page + 1)}
+					>
+						proximo
+					</Button> */}
+        </section>
+      </Content>
+    </Container>
+  );
 }
