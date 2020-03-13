01af4ffb8f495817848215a8ef76e899c483689a
LeuAlmeida <leo@webid.net.br>
2020-03-13 16:31:24 -0300

    feat(Problems): implementing functionalities

--- src/pages/Problems/index.js
+++ src/pages/Problems/index.js
@@ -1,11 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import HeaderList from '~/components/HeaderList';
+import ProblemItem from './ProblemItem';
+
+import api from '~/services/api';
 
 import { Container, Content, Grid, Button } from './styles';
 
 export default function Problems() {
   const [page, setPage] = useState(1);
+  const [problems, setProblems] = useState([]);
+
+  async function loadProblems() {
+    const response = await api.get('/delivery/problems', {
+      params: {
+        page,
+      },
+    });
+
+    setProblems(response.data);
+  }
+  useEffect(() => {
+    loadProblems();
+  }, [page]);
 
   return (
     <Container>
@@ -17,6 +34,13 @@ export default function Problems() {
             <strong>Problema</strong>
             <strong>Ações</strong>
           </section>
+          {problems.map(problem => (
+            <ProblemItem
+              updateProblems={loadProblems}
+              key={problem.id}
+              data={problem}
+            />
+          ))}
         </Grid>
         <section>
           <Button
