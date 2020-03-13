21fabf6896c697e7f509132ab0ebcb569b8fb671
LeuAlmeida <leo@webid.net.br>
2020-03-13 12:54:18 -0300

    fix(Problems): resolve conflicts

--- src/pages/Problems/index.js
+++ src/pages/Problems/index.js
@@ -1,58 +1,7 @@
-import styled from 'styled-components';
+import React from 'react';
 
-import { SimpleButton } from '~/components/utils/Button';
+// import { Container } from './styles';
 
-export const Container = styled.div`
-  display: flex;
-  justify-content: center;
-
-  padding: 0 120px;
-`;
-
-export const Content = styled.div`
-  width: 100%;
-  max-width: 1200px;
-
-  > section {
-    display: flex;
-    justify-content: space-between;
-    margin-top: 15px;
-  }
-`;
-
-export const Grid = styled.div`
-  height: 400px;
-  > section {
-    display: grid;
-
-    padding-left: 25px;
-    padding-right: 13px;
-
-    grid-template-columns: 1fr 2fr 0.5fr;
-
-    strong:last-child {
-      text-align: right;
-    }
-
-    strong {
-      font-size: 16px;
-      color: #444;
-    }
-
-    margin-bottom: 15px;
-  }
-
-  > div + div {
-    margin-top: 20px;
-  }
-`;
-
-export const Button = styled(SimpleButton)`
-  width: 100px;
-  height: 36px;
-
-  &:disabled {
-    cursor: not-allowed;
-    background: #666;
-  }
-`;
+export default function Problems() {
+  return <div />;
+}
--- src/pages/Problems/styles.js
+++ src/pages/Problems/styles.js
@@ -1,245 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import {
-  MdAdd,
-  MdChevronLeft,
-  MdChevronRight,
-  MdBlock,
-  MdCached,
-  MdError,
-} from 'react-icons/md';
+import styled from 'styled-components';
 
-import api from '~/services/api';
-import history from '~/services/history';
+import { SimpleButton } from '~/components/utils/Button';
 
-import { colors } from '~/styles/colors';
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
 
-import IconButton from '~/components/utils/Button/IconButton';
-import HeaderList from '~/components/HeaderList';
-import SearchInput from '~/components/Form/SearchInput';
+  padding: 0 120px;
+`;
 
-import RecipientItem from './RecipientItem';
+export const Content = styled.div`
+  width: 100%;
+  max-width: 1200px;
 
-import {
-  Container,
-  Content,
-  Grid,
-  ButtonSection,
-  LoadingField,
-  EmptyField,
-  Button,
-} from './styles';
+  > section {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 15px;
+  }
+`;
 
-export default function Recipients() {
-  const [page, setPage] = useState(1);
-  const [recipients, setRecipients] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+export const Grid = styled.div`
+  height: 400px;
+  > section {
+    display: grid;
 
-  async function loadRecipients() {
-    setLoading(true);
+    padding-left: 25px;
+    padding-right: 13px;
 
-    try {
-      const response = await api.get('/recipients', {
-        params: {
-          page,
-        },
-      });
+    grid-template-columns: 1fr 2fr 0.5fr;
 
-      setRecipients(response.data);
-      setLoading(false);
-    } catch (err) {
-      setError(err);
-      setLoading(false);
+    strong:last-child {
+      text-align: right;
     }
-  }
 
-  useEffect(() => {
-    loadRecipients();
-  }, [page]);
-
-  async function handleSearchRecipient(e) {
-    setLoading(true);
-    setPage(1);
-
-    try {
-      const response = await api.get('/recipients', {
-        params: {
-          q: e.target.value,
-          page,
-        },
-      });
-
-      setRecipients(response.data);
-      setLoading(false);
-    } catch (err) {
-      setError(err);
-      setLoading(false);
+    strong {
+      font-size: 16px;
+      color: #444;
     }
+
+    margin-bottom: 15px;
   }
 
-  if (loading) {
-    return (
-      <Container>
-        <Content>
-          <HeaderList title="Gerenciando destinatários">
-            <SearchInput
-              onChange={handleSearchRecipient}
-              type="text"
-              placeholder="Buscar por destinatários"
-            />
-            <IconButton
-              Icon={MdAdd}
-              title="CADASTRAR"
-              action={() => history.push('/recipients/create')}
-              type="button"
-            />
-          </HeaderList>
-          <Grid null={!recipients.length > 0}>
-            <section>
-              <strong>ID</strong>
-              <strong>Nome</strong>
-              <strong>Endereço</strong>
-              <strong>Ações</strong>
-            </section>
-            <LoadingField>
-              <MdCached size={86} color={colors.primary} />
-              <span>Carregando...</span>
-            </LoadingField>
-          </Grid>
-          <ButtonSection>
-            <Button
-              disabled={page === 1}
-              onClick={() => setPage(page - 1)}
-              type="button"
-            >
-              <MdChevronLeft size={26} />{' '}
-              <span style={{ marginRight: 5 }}>Voltar</span>
-            </Button>
-            <Button
-              disabled={recipients.length < 5}
-              type="button"
-              onClick={() => setPage(page + 1)}
-            >
-              <span style={{ marginLeft: 5 }}>Próximo</span>
-              <MdChevronRight size={26} />
-            </Button>
-          </ButtonSection>
-        </Content>
-      </Container>
-    );
+  > div + div {
+    margin-top: 20px;
   }
+`;
 
-  if (error) {
-    return (
-      <Container>
-        <Content>
-          <HeaderList title="Gerenciando destinatários">
-            <SearchInput
-              onChange={handleSearchRecipient}
-              type="text"
-              placeholder="Buscar por destinatários"
-            />
-            <IconButton
-              Icon={MdAdd}
-              title="CADASTRAR"
-              action={() => history.push('/recipients/create')}
-              type="button"
-            />
-          </HeaderList>
-          <Grid null={!recipients.length > 0}>
-            <section>
-              <strong>ID</strong>
-              <strong>Nome</strong>
-              <strong>Endereço</strong>
-              <strong>Ações</strong>
-            </section>
-            <EmptyField>
-              <MdError size={86} color={colors.primary} />
-              <span>
-                Erro ao carregar, por favor tente novamente mais tarde.
-              </span>
-            </EmptyField>
-          </Grid>
-          <ButtonSection>
-            <Button
-              disabled={page === 1}
-              onClick={() => setPage(page - 1)}
-              type="button"
-            >
-              <MdChevronLeft size={26} />{' '}
-              <span style={{ marginRight: 5 }}>Voltar</span>
-            </Button>
-            <Button
-              disabled={recipients.length < 5}
-              type="button"
-              onClick={() => setPage(page + 1)}
-            >
-              <span style={{ marginLeft: 5 }}>Próximo</span>
-              <MdChevronRight size={26} />
-            </Button>
-          </ButtonSection>
-        </Content>
-      </Container>
-    );
+export const Button = styled(SimpleButton)`
+  width: 100px;
+  height: 36px;
+
+  &:disabled {
+    cursor: not-allowed;
+    background: #666;
   }
-
-  return (
-    <Container>
-      <Content>
-        <HeaderList title="Gerenciando destinatários">
-          <SearchInput
-            onChange={handleSearchRecipient}
-            type="text"
-            placeholder="Buscar por destinatários"
-          />
-          <IconButton
-            Icon={MdAdd}
-            title="CADASTRAR"
-            action={() => history.push('/recipients/create')}
-            type="button"
-          />
-        </HeaderList>
-        <Grid null={!recipients.length > 0}>
-          <section>
-            <strong>ID</strong>
-            <strong>Nome</strong>
-            <strong>Endereço</strong>
-            <strong>Ações</strong>
-          </section>
-          {recipients.length > 0 ? (
-            recipients.map(recipient => (
-              <RecipientItem
-                updateRecipients={loadRecipients}
-                key={recipient.id}
-                data={recipient}
-              />
-            ))
-          ) : (
-            <EmptyField>
-              <MdBlock size={86} color={colors.primary} />
-              <span>Não há destinatários</span>
-            </EmptyField>
-          )}
-        </Grid>
-        <ButtonSection>
-          <Button
-            disabled={page === 1}
-            onClick={() => setPage(page - 1)}
-            type="button"
-          >
-            <MdChevronLeft size={26} />{' '}
-            <span style={{ marginRight: 5 }}>Voltar</span>
-          </Button>
-          <Button
-            disabled={recipients.length < 5}
-            type="button"
-            onClick={() => setPage(page + 1)}
-          >
-            <span style={{ marginLeft: 5 }}>Próximo</span>
-            <MdChevronRight size={26} />
-          </Button>
-        </ButtonSection>
-      </Content>
-    </Container>
-  );
-}
+`;
